b5b3df0ae58a7fefc3d460b74f33034e51c24960
lewington <lewington@student.unimelb.edu.au>
2017-11-10 21:07:18 +1100

    routwise saves updates on update"

--- app/controllers/abouts_controller.rb
+++ app/controllers/abouts_controller.rb
@@ -3,11 +3,22 @@ class AboutsController < ApplicationController
     @about = About.first
   end
 
-  def create
-
+  def update
+    @about = About.find(params[:id])
+    if @about.update_attributes(about_info)
+      flash[:success] = 'information saved'
+      redirect_to abouts_path
+    else
+      flash.now[:error] = 'invalid input'
+      render 'edit'
+    end
   end
 
   def index
     @about = About.first
+  end
+
+  def about_info
+      params.require(:about).permit(:statement, :image, :bio)
   end
 end
--- test/controllers/abouts_controller_test.rb
+++ test/controllers/abouts_controller_test.rb
@@ -10,4 +10,18 @@ class AboutsControllerTest < ActionDispatch::IntegrationTest
     get edit_about_path(1)
     assert_response :success
   end
+
+  test "create path exists" do
+    put '/abouts/1', params: {about: {statement: nil, image: nil, bio: nil}}
+    assert_response :success
+  end
+
+  test "update changes db info" do
+    bio = 'changed'
+    statement = 'different caption'
+    put "/abouts/#{1}", params: {about: {statement: statement, image: nil, bio: bio}}
+    changed_about = About.find(1)
+    assert changed_about.statement == statement
+    assert changed_about.bio == bio
+  end
 end
