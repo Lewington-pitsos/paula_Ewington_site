b95000cee280cc9a51c89b9994287bd755036af3
lewington <lewington@student.unimelb.edu.au>
2017-11-11 15:40:48 +1100

    started cat ordering scaffold

--- db/migrate/20171109212937_create_categories.rb
+++ db/migrate/20171109212937_create_categories.rb
@@ -3,6 +3,7 @@ class CreateCategories < ActiveRecord::Migration[5.1]
     create_table :categories do |t|
       t.string :title, null: false
       t.string :image
+      t.integer :position
 
       t.timestamps
     end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -7,4 +7,14 @@ class CategoryTest < ActiveSupport::TestCase
     assert cat.works
     assert cat.works.length > 1
   end
+
+  test "has a place attribute" do
+    assert Category.first.position
+  end
+
+  test "work only accepts >1 place values" do
+    assert Category.new({title: 'blah', image: nil, place: 4}).save
+    assert_not Work.new({title: 'blah', image: nil}).save
+    assert_not Work.new({title: 'blah', image: nil, place: -4}).save
+  end
 end
