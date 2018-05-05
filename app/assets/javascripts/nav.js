8738a35054fa8b2eadc135502ee331695e432010
Louka Ewignton-Pitsos <louka.ewington-itsos@estimateone.com>
2018-05-05 20:19:06 +1000

    lower navbar removed

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -2,13 +2,16 @@ $(window).on('turbolinks:load', function() {
   var $navbar = $('#only-navbar');
   var $holder = $('#nav-holder');
 
-  $holder.mouseenter(function(e) {
-    $navbar.removeClass('hidden')
-    $navbar.removeClass('fadeOutUp');
-    $navbar.addClass('fadeInDown');
-  })
-  $('#only-navbar').click(function(e) {
-    $navbar.removeClass('fadeInDown');
-    $navbar.addClass('fadeOutUp');
-  })
+  setTimeout(function() {
+    $holder.mouseenter(function(e) {
+      $navbar.removeClass('hidden')
+      $navbar.removeClass('fadeOutUp');
+      $navbar.addClass('fadeInDown');
+    })
+    $('#only-navbar').click(function(e) {
+      $navbar.removeClass('fadeInDown');
+      $navbar.addClass('fadeOutUp');
+    })
+  }, 1000);
+  
 });
